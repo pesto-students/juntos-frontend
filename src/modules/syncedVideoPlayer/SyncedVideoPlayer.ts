41a48df54adfbe0bc2d67a8b43094f1ef59a0f3d
Udhay Manhas <um@Udhays-MacBook-Pro.local>
2021-06-09 17:26:07 +0530

    Modify constructor() to create a youtube player object(sample) and start() to load the video in player

--- src/modules/syncedVideoPlayer/SyncedVideoPlayer.ts
+++ src/modules/syncedVideoPlayer/SyncedVideoPlayer.ts
@@ -18,37 +18,61 @@ class SyncedVideoPlayer {
   videoId: String
 
   /**
+   * channel - communication channel
+   */
+  channel: Socket
+
+  /**
    * room - connected party socket room name
    * Note: room and channel are the same thing in socket.io
    */
   room: String
 
-  constructor(user: User, videoId: String, room: String){
+  /**
+   * player - Youtube player object
+   */
+  player: any
+
+  constructor(user: User, videoId: String, channel: Socket, room: String){
     this.user = user;
     this.videoId = videoId;
     this.room = room;
+    this.channel = channel;
+
+    const id = 'some_id';
+    this.player = new window.YT.Player(`player-${id}`, {
+      videoId: this.videoId,
+      events: {
+        onReady: this.onPlayerReady,
+      },
+    });
+  }
+
+  onPlayerReady() {
+    // TODO
   }
   
   /**
    * start() is used to start the sync process, 
-   * once the video player is ready to play on host side, all other connected users will be notified.
+   * once the video is selected on host side, all other connected users will be notified.
    * server-side will send the notifiction to other clients in the room (messsaging to rooms is a server side feature)
    */
-  start(socket: Socket, startSecond: number){
-    socket.emit('video-ready', 
+  start(){
+    /**
+     * Notify other users
+     */
+    this.channel.emit('share-video', 
       { 
         room: this.room, 
-        videoId: this.videoId, 
-        startSecond
+        videoId: this.videoId,
       }
     )
+
+    /**
+     * Load the video in the player
+     */
+    this.player.loadVideoById(this.videoId);
   }
-
-  stop(){}
-
-  play(){}
-
-  pause(){}
 }
 
 export default SyncedVideoPlayer;
